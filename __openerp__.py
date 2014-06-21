19d8eb0a2e79083253e5c11e253b077ea096f771
tonygalmiche <tony.galmiche@infosaone.com>
2014-06-21 15:06:18 +0200

    Maj

---
+++ __openerp__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Gestion des étiquettes des colis',
+    'version': '1.0',
+    'category': 'InfoSaône',
+    'description': """
+Gestion des étiquettes des colis et des mouvements de stocks de ceux-ci
+    """,
+    'author': 'Tony GALMICHE / Asma BOUSSELMI',
+    'maintainer': 'InfoSaône',
+    'website': 'http://www.infosaone.com',
+    'depends': [],
+    'data': [],
+    'demo': [],
+    'test': [],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
