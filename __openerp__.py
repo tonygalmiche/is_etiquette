90aba6e8ce38da00c4a28e5adacf27665ba0a26d
asma-bousselmi <as.bousselmi@gmail.com>
2014-07-08 09:27:59 +0100

    __openerp__.py

--- __openerp__.py
+++ __openerp__.py
@@ -1,17 +1,21 @@
 # -*- coding: utf-8 -*-
 
 {
-    'name': 'Gestion des étiquettes des colis',
+    'name': 'Gestion des etiquettes des colis',
     'version': '1.0',
-    'category': 'InfoSaône',
+    'category': 'InfoSaone',
     'description': """
-Gestion des étiquettes des colis et des mouvements de stocks de ceux-ci
+Gestion des etiquettes des colis et des mouvements de stocks de ceux-ci
     """,
     'author': 'Tony GALMICHE / Asma BOUSSELMI',
-    'maintainer': 'InfoSaône',
+    'maintainer': 'InfoSaone',
     'website': 'http://www.infosaone.com',
-    'depends': [],
-    'data': [],
+    'depends': ['product', 'stock'],
+    'data': ['view/is_etiquette_view.xml',
+             'view/is_etiquette_sequence.xml',
+             'wizard/is_generer_etiquette_view.xml',
+             'wizard/is_changer_emplacement_view.xml',
+            ],
     'demo': [],
     'test': [],
     'installable': True,
